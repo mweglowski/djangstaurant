f38f2763a0cd87af4c59434a7c6ea868ec56baa5
Marcin Weglowski <marcin.weglowski1@gmail.com>
2023-01-20 00:05:17 +0100

    structure & styles & inventory progress

--- base/views.py
+++ base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request, "home.html")
+
+def inventory(request):
+	return render(request, "inventory.html")
